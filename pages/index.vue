488452921c21153491984b0cb4379b7cfde99445
Cynthia Dickson <cynthiaiboro@gmail.com>
2019-09-16 14:48:54 +0100

    customized freshchat pre-form

--- pages/index.vue
+++ pages/index.vue
@@ -42,7 +42,7 @@ export default {
     window.addEventListener('scroll', this.handleNavDisplay)
   var preChatTemplate = {
     //Form header color and Submit button color.
-    mainbgColor: '#0aa4db',
+    mainbgColor: '#2f6deb',
     //Form Header Text and Submit button text color.
     maintxColor: '#fff',
     //Chat Form Title
@@ -54,23 +54,23 @@ export default {
     //Fields List - Maximum is 5
     //All the values are mandatory and the script will not work if not available.
     fields : {
+      // field1 : {
+      //   //Type can be either text or title
+      //   type: "title",
+      //   //Label for Field Title, can be in any language
+      //   label: "Title",
+      //   //Field ID for Title
+      //   fieldId: "title",
+      //   //Required "yes" or "no"
+      //   required: "yes",
+      //   //Error text to be displayed
+      //   error: "Please Enter a valid Title"
+      // },
       field1 : {
-        //Type can be either text or title
-        type: "title",
-        //Label for Field Title, can be in any language
-        label: "Title",
-        //Field ID for Title
-        fieldId: "title",
-        //Required "yes" or "no"
-        required: "yes",
-        //Error text to be displayed
-        error: "Please Enter a valid Title"
-      },
-      field2 : {
         //Type for Name - Do not Change
         type: "name",
         //Label for Field Name, can be in any language
-        label: "Name",
+        label: "Fullname",
         //Default - Field ID for Name - Do Not Change
         fieldId: "name",
         //Required "yes" or "no"
@@ -78,7 +78,7 @@ export default {
         //Error text to be displayed
         error: "Please Enter a valid name"
       },
-      field3 : {
+      field2 : {
         //Type for Email - Do Not Change
         type: "email",
         //Label for Field Email, can be in any language
@@ -90,7 +90,7 @@ export default {
         //Error text to be displayed
         error: "Please Enter a valid Email"
       },
-      field4 : {
+      field3 : {
         //Type for Phone - Do Not Change
         type: "phone",
         //Label for Field Phone, can be in any language
@@ -102,24 +102,24 @@ export default {
         //Error text to be displayed
         error: "Please Enter a valid Phone Number"
       },
-      field5 : {
-        //Type for Dropdown
-        type: "dropdown",
-        //Label for Field Dropdown, can be in any language
-        label: "Plan",
-        //Field ID for Plan Dropdown
-        fieldId: "plan",
-        //Required "yes" or "no"
-        required: "yes",
-        //Error text to be displayed
-        error: "Please select an option",
-        //Options for the Dropdown field
-        options: ['Sprout','Blossom','Garden','Estate','Forest']
-      }
+      // field5 : {
+      //   //Type for Dropdown
+      //   type: "dropdown",
+      //   //Label for Field Dropdown, can be in any language
+      //   label: "Plan",
+      //   //Field ID for Plan Dropdown
+      //   fieldId: "plan",
+      //   //Required "yes" or "no"
+      //   required: "yes",
+      //   //Error text to be displayed
+      //   error: "Please select an option",
+      //   //Options for the Dropdown field
+      //   options: ['Sprout','Blossom','Garden','Estate','Forest']
+      // }
     }
   };
   window.fcSettings = {
-    token: "8b40c1c0-c70a-4b09-9563-d9648de0b98d",
+    token: "3f6ad468-e86f-4df6-902c-8c75a5a7e280",
     host: "https://wchat.freshchat.com",
     config: {
       cssNames: {
